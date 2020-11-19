db004173605740fbd1b607badbe92b1c555bfaf2
len-nguyen <70462461+len-nguyen@users.noreply.github.com>
2020-11-18 23:36:05 -1000

    fixup

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '15px', color: 'white' };
     return (
         <footer>
+          <br/>
+          <br/>
           <div style={divStyle} className="ui center aligned container">
             <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
+            Department of Information and Computer Sciences <br />
+            University of Hawaii<br />
+            Honolulu, HI 96822 <br />
             <a href="https://ics-study-hub.github.io/">Our Home Page</a>
           </div>
         </footer>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Icon, Header, Container } from 'semantic-ui-react';
+import { Grid, Header, Container, Card, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -9,29 +9,46 @@ class Landing extends React.Component {
     const headerStyle = { color: 'white' };
     return (
         <Container fluid>
-        <div className="landing-page-style">
-            <Grid id='landing-page' style={gridStyle} verticalAlign='top' textAlign='center' container>
-             <Grid.Row>
-               <Header as="h1" style={headerStyle}>Welcome to the ICS Study Hub!</Header>
-             </Grid.Row>
+          <div className="landing-page-style">
+            <Grid id='landing-page' style={gridStyle} textAlign='center' container>
+              <Grid.Row>
+                <Header as="h1" style={headerStyle} size="huge">Welcome to the ICS Study Hub!</Header>
+              </Grid.Row>
               <Grid.Row columns={3}>
                 <Grid.Column>
-                  <Icon name='calendar' size='massive' className="landing-icons"/>
+                  <Card fluid>
+                    <Image src='images/calendar1.jpg' wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>Participate in or create study sessions</Card.Header>
+                    </Card.Content>
+                  </Card>
+                  {/** <Icon name='calendar' size='massive' className="landing-icons"/> */}
                 </Grid.Column>
                 <Grid.Column>
-                  <Icon name='comment alternate' size='massive' className="landing-icons"/>
+                  <Card fluid>
+                    <Image src='images/collab.jpg' wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>Collaborate with other students.</Card.Header>
+                    </Card.Content>
+                  </Card>
+                  {/** <Icon name='comment alternate' size='massive' className="landing-icons"/> */}
                 </Grid.Column>
                 <Grid.Column>
-                  <Icon name='users' size='massive' className="landing-icons"/>
+                  <Card fluid>
+                    <Image src='images/rewards.png' wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>Earn rewards for being helpful to others</Card.Header>
+                    </Card.Content>
+                  </Card>
+                  {/** <Icon name='users' size='massive' className="landing-icons"/> */}
                 </Grid.Column>
               </Grid.Row>
-            <Grid.Row>
-                <Header as ="h2" style={headerStyle}>Here, you can give or receive help, work with others, and view questions or topics posted by other students!</Header>
+              <Grid.Row>
                 <Header as ="h3" style={headerStyle}><Link to="/signin">Login</Link> to see Current Study Sessions, Events, and other useful info!</Header>
-            </Grid.Row>
+              </Grid.Row>
             </Grid>
-        </div>
-      </Container>
+          </div>
+        </Container>
     );
   }
 }
