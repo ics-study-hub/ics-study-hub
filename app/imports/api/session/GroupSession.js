a1a6b0ab14314bd708ec76e321209a81b1a9e693
mkirts <70680675+mkirts@users.noreply.github.com>
2020-11-19 17:20:09 -1000

    Issue 8 - Mockup

---
+++ app/imports/api/session/GroupSession.js
@@ -0,0 +1,35 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection. */
+class SessionsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'SessionsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: String,
+      reason: {
+        type: String,
+        allowedValues: ['Help', 'Finding a study session', 'Exam preparation'],
+      },
+      owner: String,
+      description: String,
+      findGroup: {
+        type: String,
+        allowedValues: ['yes', 'no'],
+        defaultValue: 'yes',
+      },
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const GroupSession = new SessionsCollection();
