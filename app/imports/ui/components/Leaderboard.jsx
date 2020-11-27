1c544dad27af6c0e5c8cea0647172c857f1d81f2
len-nguyen <70462461+len-nguyen@users.noreply.github.com>
2020-11-26 19:30:21 -1000

    Update Leaderboard.jsx

--- app/imports/ui/components/Leaderboard.jsx
+++ app/imports/ui/components/Leaderboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Table, Button } from 'semantic-ui-react';
+import { Image, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
