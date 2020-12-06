4e1b034b9dde6ebe0d9118a5b5ef1ad4b5ceafe3
len-nguyen <70462461+len-nguyen@users.noreply.github.com>
2020-12-05 20:15:32 -1000

    added deletion

--- app/imports/ui/components/LeaderboardAdmin.jsx
+++ app/imports/ui/components/LeaderboardAdmin.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Button, Image, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import swal from 'sweetalert';
+import { Leaderboards } from '../../api/leaderboard/Leaderboard';
 
 /** Renders a single row in the List Stuff (Admin) table. See pages/ListStuffAdmin.jsx. */
 class LeaderboardAdmin extends React.Component {
+  handleClick(e, documentId) {
+    e.preventDefault();
+    swal({
+      title: 'Do you want to delete this person?',
+      buttons: true,
+      dangerMode: true,
+    })
+        .then((willDelete) => {
+          if (willDelete) {
+            Leaderboards.collection.remove(documentId);
+            swal('Ranking deleted');
+          }
+        });
+  }
+
   render() {
     return (
         <Table.Row>
           <Table.Cell>{this.props.leaderboard.ranking}</Table.Cell>
           <Table.Cell><Image size='mini' floated='left' src={this.props.leaderboard.image}/>{this.props.leaderboard.name}</Table.Cell>
           <Table.Cell>{this.props.leaderboard.points}</Table.Cell>
-          <Table.Cell><Button negative>&times;</Button></Table.Cell>
+          <Table.Cell><Button negative onClick={ e => this.handleClick(e, this.props.leaderboard._id)}>&times;</Button></Table.Cell>
         </Table.Row>
     );
   }
@@ -21,4 +39,4 @@ LeaderboardAdmin.propTypes = {
   leaderboard: PropTypes.object.isRequired,
 };
 
-export default LeaderboardAdmin;
+export default withRouter(LeaderboardAdmin);
