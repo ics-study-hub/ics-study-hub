2ca68c1e7061e497a62998ff760a086265cc954f
mkirts <70680675+mkirts@users.noreply.github.com>
2020-12-07 20:32:58 -1000

    Minor Changes, 11.1

--- app/imports/ui/components/UserProfile.jsx
+++ app/imports/ui/components/UserProfile.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
+import { Loader, Table } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import { UserInfo } from '../../api/userInfo/UserInfo';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class UserProfile extends React.Component {
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting Data</Loader>;
+  }
+
+  renderPage() {
     return (
         <Table.Column>
-          <Table.Cell>{this.props.profile.picture}</Table.Cell>
           <Table.Cell>{this.props.profile.firstName}</Table.Cell>
           <Table.Cell>{this.props.profile.lastName}</Table.Cell>
           <Table.Cell>{this.props.profile.email}</Table.Cell>
@@ -20,6 +25,13 @@ class UserProfile extends React.Component {
 
 UserProfile.propTypes = {
   profile: PropTypes.object.isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
-export default UserProfile;
+export default withTracker(() => {
+  const subscription = Meteor.subscribe(UserInfo.userPublicationName);
+  return {
+    users: UserInfo.collection.findOne(),
+    ready: subscription.ready(),
+  };
+})(UserProfile);
